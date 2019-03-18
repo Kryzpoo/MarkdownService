d222b1b43d8676d9ebd9311f3be0c819737bda00
Alexey Konovalov <kryzpoo@gmail.com>
2019-03-18 17:00:23 +0300

    Refactor headers and ordered lists processing

--- example/test file.md
+++ example/test file.md
@@ -12,10 +12,34 @@ header 1
 
 asdfads
 asdfads
- + text
- + text
-  + text
-  + text
- + text
- 
++ list1
+  + list 2
+  + list 2
++ list1
+  + list 3
+
+  + list 3
+    + list 4
+    + list 4
+      + list 5
+- list 6
+- list 6
+* list 7
+
++ list1
+  + list 2
+  + list 2
++ list1
+  + list 3
+
+asdf
+  + list 3
+    + list 4
+    + list 4
+      + list 5
+- list 6
+- list 6
+* list 7
+
+
 asdfa
--- processor.py
+++ processor.py
@@ -1,3 +1,4 @@
+import re
 import time
 from multiprocessing import Process
 
@@ -101,52 +102,72 @@ class MDProcessor:
             </html>
         """.format(content)
 
+    header_symbol = "#"
+    header_matcher = "^\s*" + header_symbol + "{1,6}\s+"
+
+    @classmethod
+    def _is_start_header(cls, line):
+        return re.search(cls.header_matcher, line) is not None
+
+    @classmethod
+    def _get_header_level(cls, line: str):
+        return len(line.lstrip()[:line.rfind("#")]) + 1
+
+    @classmethod
+    def _get_header_content(cls, line: str):
+        return line.lstrip(" #\t")
+
+    @classmethod
+    def _append_header(cls, html_content_lines, line, header_level):
+        html_content_lines.append("<h{level}>{line}</h{level}>".format(level=header_level,
+                                                                       line=cls._get_header_content(line)))
+
+    @classmethod
+    def _is_line_consists_of(cls, line, md_header_symbol):
+        return len(line) > 1 and line.strip().count(md_header_symbol) == len(line.strip())
+
     @classmethod
     def set_html_headers(cls, content):
         """
-        Transfrom .md headers to .html headers
+        Transform .md headers to .html headers
 
         :param content: string
         :return: string
         """
-        md_content_lines = content.splitlines()
         html_content_lines = []
 
-        for idx, line in enumerate(content.splitlines()):
+        for line in (content.splitlines()):
 
             # Processing headers starting with '#'
-            if line.lstrip().startswith("#"):
-                h_level = 1
-                for letter in line[1:]:
-                    if letter == "#":
-                        h_level += 1
-                        # Max header level is 6
-                        if h_level > 6:
-                            break
-                    elif letter == " ":
-                        break
-                if h_level > 6:
-                    # Header incorrect. Append raw line
-                    html_content_lines.append(line)
-                    break
-                html_content_lines.append("<h{h_level}>{line}</h{h_level}>".format(h_level=h_level,
-                                                                                   line=line[h_level + 1:]))
+            if cls._is_start_header(line):
+                header_level = cls._get_header_level(line)
+                cls._append_header(html_content_lines, line, header_level)
             else:
-                html_content_lines.append(line)
-
-            # Processing underlined headers
-            for md_header, html_header in (("=", "h1"), ("-", "h2")):
-                if len(line) > 1 and line.strip().count(md_header) == len(line.strip()):
-                    if idx > 0:
-                        prev_line = md_content_lines[idx - 1]
-                        html_content_lines = html_content_lines[:-2]
-                        html_content_lines.append("<{header}>{line}</{header}>".format(header=html_header,
-                                                                                       line=prev_line))
+                # Processing underlined headers
+                for md_header_symbol, header_level in (("=", "h1"), ("-", "h2")):
+                    if cls._is_line_consists_of(line, md_header_symbol):
+                        html_content_lines.pop()
+                        cls._append_header(html_content_lines, line, header_level)
+                else:
+                    # Append common line
+                    html_content_lines.append(line)
         return os.linesep.join(html_content_lines)
+
+    list_markers = ("+ ", "- ", "* ")
+    all_list_markers = "".join(list_markers)
 
     @classmethod
     def _is_start_li(cls, line):
-        return line.lstrip().startswith("+ ")
+        line_lstrip = line.lstrip()
+        for marker in cls.list_markers:
+            if line_lstrip.startswith(marker) and len(line_lstrip) > len(marker):
+                return True
+        else:
+            return False
+
+    @classmethod
+    def _get_list_marker(cls, line):
+        return line.lstrip()[0]
 
     @classmethod
     def _is_same_level(cls, level1, level2):
@@ -158,55 +179,99 @@ class MDProcessor:
 
     @classmethod
     def _append_li(cls, html_content_lines, line):
-        html_content_lines.append("<li>" + line[line.find("+ ") + 2:] + "</li>")
+        html_content_lines.append("<li>" + line.lstrip(cls.all_list_markers) + "</li>")
 
     @classmethod
-    def _start_ul(cls, md_content_lines, html_content_lines, prev_list_levels, line):
+    def _start_ul(cls, md_content_lines, html_content_lines, prev_list_levels, prev_list_marker, line):
+        """
+        Start new list/sublist cycle
+
+        :param md_content_lines: list with md content lines
+        :param html_content_lines: list with html content lines
+        :param prev_list_levels: previous levels sublists
+        :param prev_list_marker: previous list marker
+        :param line: current line of md content
+        """
         html_content_lines.append("<ul>")
+        prev_line_is_empty = False
 
         while line is not None:
             if cls._is_start_li(line):
                 cur_list_level = len(line) - len(line.lstrip())
+                cur_list_marker = cls._get_list_marker(line)
+
+                if cur_list_marker != prev_list_marker:
+                    # Other marker = end all lists
+                    for _ in prev_list_levels:
+                        html_content_lines.append("</ul>")
+
+                    # and start new list
+                    cls._start_ul(md_content_lines, html_content_lines, [cur_list_level],
+                                  cur_list_marker, line)  # Recursion
+                    return
+
                 if cls._is_same_level(cur_list_level, prev_list_levels[-1]):
+                    # Next element of the list
                     cls._append_li(html_content_lines, line)
                 else:
                     if cur_list_level in prev_list_levels:
+                        # This is the end of current sublist so stop it
                         html_content_lines.append("</ul>")
                         cls._append_li(html_content_lines, line)
+                        prev_list_levels.pop()
+
+                        # and continue previous lists
                         return
+
                     else:
+                        # This is new sublist so remember current level
                         prev_list_levels.append(cur_list_level)
-                        cls._start_ul(md_content_lines, html_content_lines, prev_list_levels, line)  # Recursion
+
+                        # And start other inner list
+                        cls._start_ul(md_content_lines, html_content_lines, prev_list_levels,
+                                      cur_list_marker, line)  # Recursion
             else:
                 if cls._is_empty_line(line):
+                    # Remember empty line to stop list if there is no elements
+                    prev_line_is_empty = True
                     html_content_lines.append(line)
-                    break
                 else:
-                    html_content_lines.append("</ul>")
+                    if prev_line_is_empty:
+                        # Stop list with all sublists
+                        for _ in prev_list_levels:
+                            html_content_lines.append("</ul>")
+
+                    # And start new paragraph
                     html_content_lines.append(line)
-                    return
+                    prev_line_is_empty = False
+
             line = next(md_content_lines, None)
+
         html_content_lines.append("</ul>")
 
     @classmethod
     def set_html_ul(cls, content):
         """
         Transform .md unordered lists to .html unordered lists
+        Using iterator and remember previous states of list marker/list levels
 
         :param content: string
         :return: string
         """
-        md_content_lines = iter(content.splitlines())
+        content_lines = content.splitlines()
         html_content_lines = []
 
-        line = True
+        md_content_lines = iter(content_lines)
+        line = ""
         while line is not None:
             line = next(md_content_lines, None)
             if line:
                 if cls._is_start_li(line):
                     cur_list_level = len(line) - len(line.lstrip())
-                    cls._start_ul(md_content_lines, html_content_lines, [cur_list_level], line)
+                    line_marker = cls._get_list_marker(line)
+                    cls._start_ul(md_content_lines, html_content_lines, [cur_list_level], line_marker, line)
                 else:
-                    html_content_lines.append("<p>" + line + "</p>")
+                    html_content_lines.append(line)
+                    html_content_lines.append("<br>")
 
         return os.linesep.join(html_content_lines)
